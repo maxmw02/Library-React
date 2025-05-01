bf051763b129c11907746e783e7b90d02226b73d
Max <mwmaxoz@gmail.com>
2025-05-01 15:16:29 -0400

    init commit heroku

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Highlight from "./ui/Highlight";
+
+function Highlights() {
+  return (
+    <section id="highlights">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Why choose <span className="purple">Library</span>
+          </h2>
+          <div className="highlight__wrapper">
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              title="Easy and quick"
+              para="Get access to the book you purchased online instantly."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="book-open" />}
+              title="10,000+ Booksk"
+              para="Library has books in all your favorite genres."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              title="Affordable"
+              para="Get your hands on books for as little as $10."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Highlights;
